365fb9e22c51e89fb3dd4130e90ae9f6333f42da
mf154290 <mf154290@students.berufskolleg-borken.de>
2022-10-19 12:24:59 +0200

    ...

---
+++ Klausuren/20221026_klausur.js
@@ -0,0 +1,8 @@
+let a ="10"
+let b = 10 
+
+console.log("Der Wert von a ist " + a)
+
+if(a === b){
+   console.log("a und b sind wertgleich")
+}
--- server.js
+++ server.js
@@ -388,6 +388,6 @@ meineApp.get('/kredit',(browserAnfrage, serverAntwort, next) => {
     }          
 })
 
-//require('./Uebungen/ifUndElse.js')
+require('./Uebungen/ifUndElse.js')
 //require('./Uebungen/klasseUndObjekt.js')
-require('./Uebungen/klausur.js')
+require('./Klausuren/20221026_klausur.js')
